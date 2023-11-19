2e146a3e049db942cb7d73ab5f6a07356a53484a
Cathyr34 <99675854+Cathyr34@users.noreply.github.com>
2023-11-19 11:06:53 +0100

    gestion des points dans js

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -22,46 +22,63 @@ let arrowRight = document.querySelector('.arrow_right');
 
 let currentIndex = 0;
 
-function updateDots() {
-  const dots = document.querySelectorAll('.dot');
-  dots.forEach((dot, index) => {
-    if (index === currentIndex) {
-      dot.classList.add('dot_selected');
-    } else {
-      dot.classList.remove('dot_selected');
+//ajoute la classe ‘dot_selected’ à l’élément dont l’index est égal à currentIndex, et supprime cette classe des autres éléments ‘dot’
+
+var dotsContainer = document.querySelector('.dots');
+dotsContainer.innerHTML = ''; // Supprime les points existants
+
+for (var i = 0; i < slides.length; i++) {
+    var dot = document.createElement('div');
+    dot.className = 'dot';
+    if (i == currentIndex) {
+        dot.classList.add('dot_selected');
     }
-  });
+    dotsContainer.appendChild(dot);
 }
+
+// Ajoute des écouteurs d'événements
+  	document.querySelector('.arrow_right').addEventListener('click', nextSlide);
+  	document.querySelector('.arrow_left').addEventListener('click', previousSlide);
 
 function updateSlide() {
 	const slide = slides[currentIndex];
 	const imgElement = document.querySelector('.banner-img');
 	const textElement = document.querySelector('#banner p');
   
-	// Met à jour l'image
+// Met à jour l'image
 	imgElement.src = "./assets/images/slideshow/" + slide.image;
   
-	// Met à jour le texte
+// Met à jour le texte
 	textElement.innerHTML = slide.tagLine;
   
-	// Met à jour les points
-	updateDots();
-  }
+// Met à jour les points
+	var dots = document.querySelectorAll('.dot');
+    for (var i = 0; i < dots.length; i++) {
+        if (i == currentIndex) {
+            dots[i].classList.add('dot_selected');
+        } else {
+            dots[i].classList.remove('dot_selected');
+        }
+    }
+}
   
-  // Met à jour le slide
-  updateSlide();
+// Met à jour le slide
+  	updateSlide();
 
-  function nextSlide() {
-	currentIndex = (currentIndex + 1) % slides.length;
-	updateSlide();
-  }
-  
-  function previousSlide() {
-	currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-	updateSlide();
-  }
-  
-  // Ajoute des écouteurs d'événements
-  document.querySelector('.arrow_right').addEventListener('click', nextSlide);
-  document.querySelector('.arrow_left').addEventListener('click', previousSlide);
+function nextSlide() {
+    currentIndex++;
+    if (currentIndex == slides.length) {
+        currentIndex = 0;
+    }
+    console.log('nextSlide a été appelé, currentIndex est maintenant : ' + currentIndex);
+    updateSlide();
+}
 
+function previousSlide() {
+    currentIndex--;
+    if (currentIndex < 0) {
+        currentIndex = slides.length - 1;
+    }
+    console.log('previousSlide a été appelé, currentIndex est maintenant : ' + currentIndex);
+    updateSlide();
+}
